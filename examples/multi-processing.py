44388ebc23497465ea2d1c1e391392f4e08857aa
yu45020 <yu45020@gmail.com>
2018-06-06 21:03:33 -0700

    add an example
    Former-commit-id: 7b06917b69086ce621f78d28ce5155689dc42ee6 [formerly 3940cbf2c0b208279ad5b572a9890d1f519a6fea]
    Former-commit-id: 97625e6e23875a846ac38de6b0c19eb641c9c3f3

---
+++ examples/multi-processing.py
@@ -0,0 +1,34 @@
+import glob
+import time
+from multiprocessing import Pool, cpu_count
+
+from torchvision.utils import save_image
+
+from utils.ImageSplitter import *
+
+# pre_train = './model_results/DCSCN_model_387epos.pt'
+# model = DCSCN(color_channel=3,
+#               up_scale=2,
+#               feature_layers=12,
+#               first_feature_filters=196,
+#               last_feature_filters=48,
+#               reconstruction_filters=128,
+#               up_sampler_filters=32)
+# model.load_state_dict(torch.load(pre_train, map_location='cpu'))
+model = UpConv_7()
+model.load_pre_train_weights('./model_check_points/Upconv_7/noise1_scale2.0x_model.json')
+
+lr_imgs = glob.glob("./benchmark/Set14_SR/lr_imgs/*_LR.png")
+
+if __name__ == '__main__':
+    # model = torch.load("dcscn.pt")  # set the img_pad=0
+    sp = ImageSplitter(seg_size=48, scale_factor=2, scale_method=None, boarder_pad_size=3, img_pad=model.offset // 2)
+    for lr_img in lr_imgs:
+        a = time.time()
+        patches = sp.split_img_tensor(Image.open(lr_img))
+        with Pool(cpu_count()) as p:
+            out = p.map(model.forward_checkpoint, patches)
+        final = sp.merge_img_tensor(out)
+        save_image(final, lr_img + "waifu.png", padding=0)
+        print(time.time() - a)
+#
