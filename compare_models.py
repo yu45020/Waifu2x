444e0cb6e54e031de864efbe80cc57cab7b5ba02
yu45020 <yu45020@gmail.com>
2018-06-12 00:59:39 -0700

    Add model comparison. fix DCSCN model checkpoint
    Former-commit-id: 2c4ea69a1b1d9eb070f6fe3fef220e06ec3d66c4 [formerly 26eefcd6cce9e0ae9345e08f460afa4fa1f6c548]
    Former-commit-id: 7b3cfd2150bdac1a383c6186725d21f49df04580

--- compare_models.py
+++ compare_models.py
@@ -1,13 +1,42 @@
-import matplotlib.pyplot as plt
 import torch
+from PIL import Image
+from torchvision.utils import save_image
+import time
+from models import DCSCN, UpConv_7
+from utils.prepare_images import *
+from multiprocessing import Pool, cpu_count
 
-loss_espcn_7 = torch.load("./model_results/ESPCN_7_6epos_loss.pk")
-loss_espcn_7_nous = torch.load("./model_results/ESPCN_7_6epos_loss_noSU.pk")
+DCSCN_12 = "model_check_points/DCSCN/DCSCN_model_387epos_L12_noise_1.pt"
+model_dcscn = torch.load(DCSCN_12)
 
-plt.plot(loss_espcn_7, color='red', label='With Selection Unit')
-plt.plot(loss_espcn_7_nous, color='green', label='No Selection Unit')
-plt.title('ESPCN_7 Model 6 Epoches')
-plt.xlabel('Per 50 images (96x96 -> 192x192)')
-plt.ylabel('Weighted MSE Loss')
-plt.legend()
-plt.show()
+model_upconv7 = UpConv_7()
+model_upconv7.load_pre_train_weights("model_check_points/Upconv_7/anime/noise0_scale2.0x_model.json")
+
+demo_img = 'demo_imgs/sp_twitter_icon_ao_3.png'
+
+img = Image.open(demo_img).convert("RGB")
+img_bicubic = img.resize((img.size[0] * 2, img.size[1] * 2), Image.BICUBIC)
+img_bicubic = to_tensor(img_bicubic).unsqueeze(0)
+
+if __name__ == '__main__':
+    print("Demo image size : {}".format(img.size))
+    img_splitter = ImageSplitter(seg_size=48, scale_factor=2, boarder_pad_size=3)
+
+    print("Runing DCSCN models...")
+    start_t = time.time()
+    img_patches = img_splitter.split_img_tensor(img, scale_method=Image.BILINEAR, img_pad=0)
+    with Pool(cpu_count()) as p:
+        out = p.map(model_dcscn.forward_checkpoint, img_patches)
+    img_dcscn = img_splitter.merge_img_tensor(out)
+    print(" DCSCN_12 model runtime: {:.3f}".format(time.time() - start_t))
+    # 20s
+
+    start_t = time.time()
+    img_patches = img_splitter.split_img_tensor(img, scale_method=None, img_pad=model_upconv7.offset)
+    with Pool(cpu_count()) as p:
+        out = p.map(model_upconv7.forward_checkpoint, img_patches)
+    img_upconv7 = img_splitter.merge_img_tensor(out)
+    print("Upconv_7 runtime: {:.3f}".format(time.time() - start_t))
+    # 5.3s
+    final = torch.cat([img_bicubic, img_dcscn, img_upconv7])
+    save_image(final, 'Readme_imgs/demo_bicubic_dcscn_upconv.png', nrow=3)
